49fde431b2e2361649d0070ae27da1de73273e42
royhandf <royhandaffa14@gmail.com>
2024-03-07 10:52:23 +0700

    feat: role access

---
+++ src/const/access.js
@@ -0,0 +1,54 @@
+export const accessCategories = {
+  lihat: ["organizer"],
+  tambah: ["organizer"],
+  edit: ["organizer"],
+  hapus: ["organizer"],
+};
+
+export const accessTalents = {
+  lihat: ["organizer"],
+  tambah: ["organizer"],
+  edit: ["organizer"],
+  hapus: ["organizer"],
+};
+
+export const accessEvents = {
+  lihat: ["organizer"],
+  tambah: ["organizer"],
+  edit: ["organizer"],
+  hapus: ["organizer"],
+};
+
+export const accessOrganizers = {
+  lihat: ["owner"],
+  tambah: ["owner"],
+  edit: ["owner"],
+  hapus: ["owner"],
+};
+
+export const accessParticipant = {
+  lihat: ["organizer"],
+  tambah: ["organizer"],
+  edit: ["organizer"],
+  hapus: ["organizer"],
+};
+export const accessPayments = {
+  lihat: ["organizer"],
+  tambah: ["organizer"],
+  edit: ["organizer"],
+  hapus: ["organizer"],
+};
+
+export const accessOrders = {
+  lihat: ["organizer", "admin", "owner"],
+  tambah: ["organizer", "admin", "owner"],
+  edit: ["organizer", "admin", "owner"],
+  hapus: ["organizer", "admin", "owner"],
+};
+
+export const accessAdmin = {
+  lihat: ["organizer"],
+  tambah: ["organizer"],
+  edit: ["organizer"],
+  hapus: ["organizer"],
+};
